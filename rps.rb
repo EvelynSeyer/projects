5fad7598ffcfc926a4781b5e5c4a56e1229eae93
Chris W <chris06v1@gmail.com>
2015-12-04 20:02:31 -0800

    Added ability to get user input and validates it

--- rps.rb
+++ rps.rb
@@ -1,2 +1,15 @@
-puts 'Lets play Rock Paper Scissors'
-user_input = gets.chomp
+ary = ["rock", "paper", "scissors"]
+prng = Random.new
+puts 'Lets play Rock Paper Scissors! Pick rock, paper, or scissors?'
+user_input = gets.chomp.downcase.strip
+chosen = prng.rand(0..ary.length)
+computer_picks = ary[chosen]
+
+while !ary.include?(user_input)
+  puts "Invalid answer!"
+  puts 'Lets play Rock Paper Scissors! Pick rock, paper, or scissors?'
+  user_input = gets.chomp.downcase.strip
+end
+
+puts computer_picks
+puts user_input
